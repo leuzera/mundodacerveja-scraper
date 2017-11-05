ebd95a051a5aeecceeb7526b0e5c0eedfcd5be7b
theleuzera <fleuryleomoraes@gmail.com>
2017-11-05 15:33:04 -0200

    scrapy.log está depreciado

--- mundo_da_cerveja/mundo_da_cerveja/pipelines.py
+++ mundo_da_cerveja/mundo_da_cerveja/pipelines.py
@@ -7,7 +7,6 @@
 import pymongo
 from scrapy.conf import settings
 from scrapy.exceptions import DropItem
-from scrapy import log
 
 
 class MundoDaCervejaPipeline(object):
@@ -33,7 +32,5 @@ class MongoDBPipeline(object):
                 raise DropItem("Missing {0}!".format(data))
         if valid:
             self.collection.insert(dict(item))
-            log.msg("Beer added to MongoDB database!",
-                    level=log.DEBUG, spider=spider)
 
         return item
